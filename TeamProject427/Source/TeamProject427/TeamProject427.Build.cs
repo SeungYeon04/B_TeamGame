69cfb54b8271635ea4fbc7b723410d4f2f4e6456
epsl.kwm@gmail.com <100332811+SeungYeon04@users.noreply.github.com>
2024-12-23 12:55:02 +0900

    .

--- TeamProject427/Source/TeamProject427/TeamProject427.Build.cs
+++ TeamProject427/Source/TeamProject427/TeamProject427.Build.cs
@@ -8,6 +8,6 @@ public class TeamProject427 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "OnlineSubsystemSteam", "OnlineSubsystem", "OnlineSubsystemUtils" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "OnlineSubsystemSteam", "OnlineSubsystem" });
     }
 }
